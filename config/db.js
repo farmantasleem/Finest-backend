e96f04779c45e5712b4e8ff9ee018465bd783371
unknown <iamfarmanmalik@gmail.com>
2022-12-16 15:04:18 +0530

    fie updated

--- config/db.js
+++ config/db.js
@@ -2,10 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDatabase = () => {
   mongoose
-    .connect(process.env.DB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+    .connect(process.env.DB_URL)
     
     
 };
