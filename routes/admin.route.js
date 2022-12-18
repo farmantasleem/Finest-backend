84b155039c62d8f15790c0a920f83861d484e397
unknown <iamfarmanmalik@gmail.com>
2022-12-19 02:09:14 +0530

    file updated

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -222,7 +222,8 @@ adminRoute.patch("/user/:userid2",Authentication,async(req,res)=>{
         const user=await Usermodel.findOne({_id:userid});
         if(user?._id){
             if(user?.role=="admin"){
-                await Usermodel.findOneAndUpdate({_id:userid2},{role:user.role=="admin"?"user":"admin"})
+                const update_role=user.role=="admin"?"user":"admin"
+                await Usermodel.findOneAndUpdate({_id:userid2},{role:update_role})
                 res.status(200).send({msg:"He's Admin Now"})
             }else{
                 res.status(404).send({"msg":"Not authenticated"})
